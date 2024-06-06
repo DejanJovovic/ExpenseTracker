a0e173c2761bcf8a2b911cc67a71b82296c035a9
Dejan Jovovic <dejan.jovovic2002@gmail.com>
2024-06-06 12:15:25 +0200

    removing expenses(by swiping away) and also let the user bring back the removed expense with the use of ScaffoldMessenger and SnackBars

--- expense_tracker/lib/widget/expenses.dart
+++ expense_tracker/lib/widget/expenses.dart
@@ -3,74 +3,112 @@ import 'package:expense_tracker/widget/new_expense.dart';
 import 'package:flutter/material.dart';
 import 'package:expense_tracker/models/expense.dart';
 
-class Expenses extends StatefulWidget{
-
+class Expenses extends StatefulWidget {
   const Expenses({super.key});
 
-@override
+  @override
   State<Expenses> createState() {
     return _ExpensesState();
   }
-
 }
 
-class _ExpensesState extends State<Expenses>{
+class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      title: 'Flutter Course',
+      amount: 19.99,
+      date: DateTime.now(), // uses today and now as the data that is stored
+      category: Category.work,
+    ),
+    Expense(
+      title: 'Cinema',
+      amount: 15.69,
+      date: DateTime.now(), // uses today and now as the data that is stored
+      category: Category.leisure,
+    ),
+  ];
 
-final List<Expense> _registeredExpenses = [
-  Expense(title: 'Flutter Course',
-  amount: 19.99, 
-  date: DateTime.now(), // uses today and now as the data that is stored
-  category: Category.work,
-  ),
-  Expense(title: 'Cinema',
-  amount: 15.69, 
-  date: DateTime.now(), // uses today and now as the data that is stored
-  category: Category.leisure,
-  ),
-];
-
-
-void _openAddExpenseOverlay(){
-  showModalBottomSheet( // a pop up window
-    isScrollControlled: true, // with this the pop up screen will take all the space
-    context: context,
-    builder: (ctx) => // here we display what happens when the plus button is clicked and user is moved to the next screen
-      NewExpense(onAddExpense: _addExpense),
+  void _openAddExpenseOverlay() {
+    showModalBottomSheet(
+      // a pop up window
+      isScrollControlled:
+          true, // with this the pop up screen will take all the space
+      context: context,
+      builder:
+          (ctx) => // here we display what happens when the plus button is clicked and user is moved to the next screen
+              NewExpense(onAddExpense: _addExpense),
     );
-}
+  }
 
-void _addExpense(Expense expense){
+  void _addExpense(Expense expense) {
+    setState(() {
+      _registeredExpenses.add(expense);
+    });
+  }
 
-  setState(() {
-    _registeredExpenses.add(expense);
-  });
-  
-}
+  void _removeExpense(Expense expense) {
+    final expenseIndex = _registeredExpenses
+        .indexOf(expense); // which index was the expense that was removed
 
+    setState(() {
+      _registeredExpenses.remove(expense);
+    });
 
-@override
-  Widget build(BuildContext context) { 
-    return Scaffold( // it sends the background of the app to white by default
-      appBar: AppBar( // to add appbar at the top of the screen
+    ScaffoldMessenger.of(context).clearSnackBars(); // which removes any snackBars/info messages we might still have on the screen
+    // snackBar is info message shown on the screen
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        // ScaffoldMessenger controls the user interface and shows the certain things on the UI
+        duration: const Duration(
+            // duration means for how long this snackBar is shown
+            seconds: 3),
+        content: const Text("Expense deleted."),
+        action: SnackBarAction(
+          label: "Undo",
+          onPressed: () {
+            // define the action that should happen if the "Undo" button above is pressed
+            setState(() {
+              // updates the UI
+              _registeredExpenses.insert(
+                  // this insert brings back the removed expense by tapping on "Undo"
+                  expenseIndex,
+                  expense);
+            });
+          },
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget mainContent = const Center(
+      // if there aren't any expenses shown on the screen this text is shown
+      child: Text("No expenses found. Start adding some!"),
+    );
+
+    if (_registeredExpenses.isNotEmpty) {
+      // if there are just show the expenses as normal by calling the mainContent below in the Column widget
+      mainContent = ExpensesList(
+          expenses: _registeredExpenses, onRemoveExpense: _removeExpense);
+    }
+
+    return Scaffold(
+      // it sends the background of the app to white by default
+      appBar: AppBar(
+        // to add appbar at the top of the screen
         title: const Text('Flutter ExpenseTracker'),
         actions: [
           IconButton(
-            onPressed: _openAddExpenseOverlay, 
-            icon: const Icon(Icons.add))
+              onPressed: _openAddExpenseOverlay, icon: const Icon(Icons.add))
         ],
       ),
       body: Column(
         children: [
           const Text('The chart'),
-          Expanded(
-            child: ExpensesList(
-            expenses: _registeredExpenses),
-          ),
-
+          Expanded(child: mainContent),
         ],
-        ),
-    ); 
-  
+      ),
+    );
   }
-
 }
--- expense_tracker/lib/widget/expenses_list/expenses_list.dart
+++ expense_tracker/lib/widget/expenses_list/expenses_list.dart
@@ -2,22 +2,31 @@ import 'package:expense_tracker/widget/expenses_list/expense_item.dart';
 import 'package:flutter/material.dart';
 import 'package:expense_tracker/models/expense.dart';
 
-class ExpensesList extends StatelessWidget{
-
+class ExpensesList extends StatelessWidget {
   const ExpensesList({
     super.key,
     required this.expenses,
-    }); 
+    required this.onRemoveExpense,
+  });
 
   final List<Expense> expenses;
+  final void Function(Expense expense) onRemoveExpense;
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder( // scrollable list and build create items only when they are visible
+    return ListView.builder(
+      // scrollable list and build create items only when they are visible
       itemCount: expenses.length,
-      itemBuilder: (ctx, index) => // immediately define the value that should be returned
-      ExpenseItem(expenses[index])
+      // immediately define the value that should be returned
+      itemBuilder: (ctx, index) => Dismissible(
+          // allows the user to dismiss the expense by swiping away
+          key: ValueKey(// creates a key object that can be used as key value
+              expenses[index]),
+          onDismissed: (direction) // this is what happens when the expense is dismissed or swiped away
+            { 
+          onRemoveExpense(expenses[index]);
+          },
+          child: ExpenseItem(expenses[index])),
     );
   }
-
 }
